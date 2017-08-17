c0eecb20ee37def7a71c411df409b1eff7a3ffd9
nigelASC4 <nigelbk1@gmail.com>
2017-08-16 22:33:46 -0400

    Add files via upload

---
+++ demoday/demoday/script4.js
@@ -0,0 +1,19 @@
+var topOptions=new Vue({
+  el: '#demo',
+  data: {
+    show: true
+  }
+})
+
+
+
+
+var about = new Vue({
+  el: '#about',
+  data: {
+    Header: "About me",
+    about:"Growing up in my househould, I was always taught the value of hardwork."+
+    "I am currently a rising senior at A. Philip Randolph Campus High school"+
+    "and am aiming to study engineering in a top college. ",
+  }
+})
